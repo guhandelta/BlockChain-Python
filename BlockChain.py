b84099ba0a81dcfd602fce58c0c251efe492e6b7
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-08-01 16:39:51 -0400

    Constructed basic Blockchain

--- BlockChain.py
+++ BlockChain.py
@@ -2,9 +2,12 @@ blockchain = []
 
 def get_last_blockchain_value():
     #Returns teh last value of the current Blockchain
-    return blockchain[-1]
+    if len(blockchain) < 1:
+        return None
+    return blockchain[-1] # if the if condition is met, the fn() is quit automatically and so-
+    #- this statement is not included in an else block
 
-def add_value(transaction_amount, last_transaction=[1]):
+def add_transaction(transaction_amount, last_transaction=[1]): #last_transaction=[1] is not removed as it is a required param
     """
         Append a new block into the blockchain
 
@@ -13,20 +16,41 @@ def add_value(transaction_amount, last_transaction=[1]):
             2) last_transacrion : The last transaction in the blockchian => default([1])
 
     """
+    if last_transaction == None:
+        last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
-def get_user_input():
+def get_transaction_value():
     #Returns the user input as a float => float() is used to convert the string into float, as the I/P value passed into input()--
     #-- is treated as text
-    return float(input('Transaction Amount:'))
+    user_input = float(input('Transaction Amount:'))
+    return user_input
 
-tx_amount = get_user_input()
-add_value(tx_amount)
+def get_user_choice():
+    user_input = input('User Choice:\t')
+    return user_input
 
-tx_amount = get_user_input()
-add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
+def print_blockchain():
+ #Output the Blockchain list
+    for block in blockchain:
+        print("Printing the Blockchain", block)    
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
+# Get the 1st transaction and add it to the blockchain
+tx_amount = get_transaction_value()
+add_transaction(tx_amount)
 
-print(blockchain)
+while True:
+    print("Make your choice \n1) Add a new Transaction \n2) Output the blockchain \nq) Quit" )
+    user_choice = get_user_choice() 
+
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain()
+    elif user_choice=='q':
+        break
+    else:
+        print("Invalid Entry")
+
+print("Done!")

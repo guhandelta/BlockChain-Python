b1578e1694209e3a8696b2aefcab069e686d4e1c
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-08-01 18:01:08 -0400

    Completed a Basic architecture of a Blockchain

--- BlockChain.py
+++ BlockChain.py
@@ -1,5 +1,6 @@
 blockchain = []
 
+#Function to retrieve the previous block's value
 def get_last_blockchain_value():
     #Returns teh last value of the current Blockchain
     if len(blockchain) < 1:
@@ -7,6 +8,7 @@ def get_last_blockchain_value():
     return blockchain[-1] # if the if condition is met, the fn() is quit automatically and so-
     #- this statement is not included in an else block
 
+#Function to add a new transaction
 def add_transaction(transaction_amount, last_transaction=[1]): #last_transaction=[1] is not removed as it is a required param
     """
         Append a new block into the blockchain
@@ -20,34 +22,41 @@ def add_transaction(transaction_amount, last_transaction=[1]): #last_transaction
         last_transaction = [1]
     blockchain.append([last_transaction, transaction_amount])
 
+#Function to recieve the Transaction value
 def get_transaction_value():
     #Returns the user input as a float => float() is used to convert the string into float, as the I/P value passed into input()--
     #-- is treated as text
-    user_input = float(input('Transaction Amount:'))
+    user_input = float(input('Transaction Amount:\t'))
     return user_input
 
+#Function to get the Users's choice of action
 def get_user_choice():
     user_input = input('User Choice:\t')
     return user_input
 
+#Function to print the entire blockchain
 def print_blockchain():
  #Output the Blockchain list
+    print('-' * 20, '\n The Blockchain: \n')
     for block in blockchain:
         print("Printing the Blockchain", block)    
+    else:
+        print('-' * 20)
 
+#Function to verify if the chain is valid
 def verify_chain():
     block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1 # Security feature for checking the previous block for 1st node
+            #block_index += 1 # Security feature for checking the previous block for 1st node
             continue #skip the following lines and proceed with the iteration
-        if block[0] == blockchain[block_index -1]:
+        if blockchain[block_index][0] == blockchain[block_index -1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index += 1
+    #     block_index += 1
     return is_valid
             
 
@@ -55,7 +64,9 @@ def verify_chain():
 tx_amount = get_transaction_value()
 add_transaction(tx_amount)
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("Make your choice \n1) Add a new Transaction \n2) Output the blockchain \nh) Manipulate Blockchain \nq) Quit" )
     user_choice = get_user_choice() 
 
@@ -69,11 +80,14 @@ while True:
             blockchain[0] = [2]
         print("Cannot Manipulate a Block")
     elif user_choice=='q':
-        break
+        waiting_for_input = False
     else:
         print("Invalid Entry")
     if not verify_chain():
+        print_blockchain()
         print("Invalid Entry")
         break
+else:
+    print("User Left")
 
 print("Done!")
